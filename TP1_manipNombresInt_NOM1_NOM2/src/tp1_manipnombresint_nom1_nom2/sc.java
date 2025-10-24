54fc00f30614c5e7cbe7f0c1b60ab3aaca32f58e
stell <stell@10.3.62.98>
2025-10-24 12:19:22 +0200

    Création d'un nouveau projet versionné

---
+++ TP1_manipNombresInt_NOM1_NOM2/src/tp1_manipnombresint_nom1_nom2/sc.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp1_manipnombresint_nom1_nom2;
+
+/**
+ *
+ * @author stell
+ */
+class sc {
+
+    static int nextInt() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
